5f7d652259a348b0569dee2400b9301a1bd59fb0
ales0sa <alesosa@gmail.com>
2019-09-16 18:37:24 -0300

    Login animado
    Pagina de login

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="es" >
+<head>
+  <meta charset="UTF-8">
+  <title>LAVADERO | Turnos On-Line</title>
+  <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+
+
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
+
+
+</head>
+<body>
+
+
+<div class="container">
+  <div class="left-section">
+    <div class="header">
+      <h1 class="animation a1">Bienvenido!</h1>
+      <h4 class="animation a2">Ingresa aquí para sacar tu turno.</h4>
+    </div>
+    <div class="form">
+      <input type="email" class="form-field animation a3" placeholder="PATENTE">
+      <input type="password" class="form-field animation a4" placeholder="D.N.I.">
+      <p class="animation a5"><a href="#">Llama por telefono</a></p>
+      <button class="animation a6">Sacar turno</button>
+    </div>
+  </div>
+  <div class="right-section"></div>
+</div>
+  
+</body>
+</html>
